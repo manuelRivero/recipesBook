22bb368b7e9330a62d9d5175ef0044ad423693ce
manuel rivero <manuel.enrique.r.v@gmail.com>
2020-04-25 14:49:39 -0300

    [add] recipes form basic behabior

--- src/app/components/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/components/recipes/recipe-edit/recipe-edit.component.ts
@@ -7,53 +7,93 @@ import { Recipe } from "../../shared/recipes.model/recipes.model";
 @Component({
   selector: "app-recipe-edit",
   templateUrl: "./recipe-edit.component.html",
-  styleUrls: ["./recipe-edit.component.css"]
+  styleUrls: ["./recipe-edit.component.css"],
 })
 export class RecipeEditComponent implements OnInit {
   id: string;
   edit;
   editForm: FormGroup;
   recipe: Recipe;
-  activeStep:number=0;
-  steps:string[]=["Cool name", "Ingredients", "Instructions", "Done"]
-  readOnly:boolean=false;
+  activeStep: number = 0;
+  steps: string[] = ["Cool name", "Ingredients", "Instructions", "Done"];
+  readOnly: boolean = false;
+  loading: boolean = false;
+  directionright: boolean = true;
+  images = {
+    default: "assets/images/img-placeholder.jpg",
+    loading: "assets/images/loader.gif",
+    error: "assets/images/error.jpg",
+  };
+
+  activeImage;
+
   constructor(
     private route: ActivatedRoute,
     public _recipeService: RecipeService,
     private router: Router
   ) {}
 
-  onNextStep(){
-    this.activeStep +=1;
-    if(this.activeStep >= this.steps.length -1){
-      this.readOnly = true
-    }
-
+  onabort() {
+    this.editForm.get("step0").get("imgUrl").setErrors({ incorrect: true });
+    this.activeImage = this.images.error;
   }
-  onPrevStep(){
-    if(this.activeStep <= this.steps.length -1 || this.activeStep >= 0){
-      this.activeStep -=1;
-      this.readOnly=false
-    }
 
+  imgLoad(){
+    if(this.activeImage === this.images.error){
+      return null;
+    }
+    this.editForm.get("step0").get("imgUrl").setErrors(null)
+  }
+
+  getImg() {
+
+    let imageInput = this.editForm.get("step0").get("imgUrl");
+    if (!this.edit) {
+      this.activeImage = this.images.default;
+    }
+    if (imageInput.errors) {
+      this.activeImage = this.images.error;
+    }else {
+       this.activeImage = imageInput.value;
+    }
+  }
+
+  onNextStep() {
+    this.activeStep += 1;
+    this.directionright = true;
+    if (this.activeStep >= this.steps.length - 1) {
+      this.readOnly = true;
+    }
+  }
+  onPrevStep() {
+    this.directionright = false;
+    if (this.activeStep <= this.steps.length - 1 || this.activeStep >= 0) {
+      this.activeStep -= 1;
+      this.readOnly = false;
+    }
   }
 
   onSubmit() {
-    const { name, imgUrl, description, ingredients } = this.editForm.value;
-    let newRecipe = new Recipe(name, description, imgUrl, ingredients, this.id);
-    this.activeStep= this.steps.length;
+    const { name, description, imgUrl} = this.editForm.get('step0').value;
+    const ingredients = this.editForm.get('step1').value;
+    const instructions = this.editForm.get('step2').value;
+    let newRecipe = new Recipe(name, description, instructions, imgUrl, ingredients, this.id);
+    this.activeStep = this.steps.length;
+    this.loading = true;
     if (this.edit) {
-      this._recipeService.updateRecipe(newRecipe).subscribe( res =>{
-        this.onCancel()
-      })
+      this._recipeService.updateRecipe(newRecipe).subscribe((res) => {
+        this.loading = false;
+        this.onCancel();
+      });
     } else {
-      this._recipeService.addRecipe(this.editForm.value).subscribe(res => {
-        this.onCancel()
-      })
+      this._recipeService.addRecipe(newRecipe).subscribe((res) => {
+        this.loading = false;
+        this.onCancel();
+      });
     }
   }
-  onCancel(){
-    this.router.navigate(["/recipes"])
+  onCancel() {
+    this.router.navigate(["/recipes"]);
   }
   onNewIngredient(): void {
     (<FormArray>this.editForm.get("step1")).push(
@@ -61,25 +101,30 @@ export class RecipeEditComponent implements OnInit {
         name: new FormControl(null, Validators.required),
         amount: new FormControl(null, [
           Validators.required,
-          Validators.pattern(/^[1-9]+[0-9]*$/)
-        ])
+          Validators.pattern(/^[1-9]+[0-9]*$/),
+        ]),
       })
     );
   }
 
   onNewStep(): void {
+
     (<FormArray>this.editForm.get("step2")).push(
       new FormGroup({
-        step: new FormControl(null, Validators.required)
+        description: new FormControl(null, Validators.required),
       })
     );
   }
 
   onDeleteIngredient(ingredientIndex: number): void {
-    (<FormArray>this.editForm.get("ingredients")).removeAt(ingredientIndex);
+    (<FormArray>this.editForm.get("step1")).removeAt(ingredientIndex);
+  }
+  ondeleteIntruction(instructionIndex: number):void {
+    (<FormArray>this.editForm.get("step2")).removeAt(instructionIndex);
+
   }
   ngOnInit() {
-    this.route.params.subscribe(params => {
+    this.route.params.subscribe((params) => {
       this.edit = params["id"] != null;
       if (this.edit) {
         this.id = params["id"];
@@ -93,15 +138,21 @@ export class RecipeEditComponent implements OnInit {
   }
 
   formInit() {
+    this.activeImage = this.images.default;
     let name = "";
     let imgUrl = "";
-    let description = new FormArray([], Validators.required);
+    let description = "";
+    let instructions = new FormArray([], Validators.required);
     let ingredients = new FormArray([], Validators.required);
+
+
     if (this.recipe) {
+
       name = this.recipe.name;
       imgUrl = this.recipe.imgpath;
-      // set up description to be an formControl array
-     // description = this.recipe.description;
+      description = this.recipe.description;
+      this.activeImage = this.recipe.imgpath;
+
       if (this.recipe["ingredients"]) {
         for (let i of this.recipe.ingredients) {
           ingredients.push(
@@ -109,19 +160,29 @@ export class RecipeEditComponent implements OnInit {
               name: new FormControl(i.name, Validators.required),
               amount: new FormControl(i.amount, [
                 Validators.required,
-                Validators.pattern(/^[1-9]+[0-9]*$/)
-              ])
+                Validators.pattern(/^[1-9]+[0-9]*$/),
+              ]),
             })
           );
         }
       }
+      for (let i of this.recipe.instructions) {
+        instructions.push(
+          new FormGroup({
+            description: new FormControl(i.description, Validators.required)
+          })
+          )
+      }
     }
 
     this.editForm = new FormGroup({
-      step0: new FormGroup( {name : new FormControl(name, Validators.required), imgUrl :new FormControl(imgUrl, Validators.required)}),
+      step0: new FormGroup({
+        name: new FormControl(name, Validators.required),
+        imgUrl: new FormControl(imgUrl, Validators.required),
+        description:new FormControl(description, Validators.required)
+      }),
       step1: ingredients,
-      step2: description
-
+      step2: instructions,
     });
   }
 }
--- src/app/components/shared/recipes.model/recipes.model.ts
+++ src/app/components/shared/recipes.model/recipes.model.ts
@@ -2,7 +2,8 @@ import { Ingredients } from './../ingredients.model/ingredients.model';
 export class Recipe{
     constructor(
         public name:string,
-        public description:string[],
+        public description:string,
+        public instructions:[{description:string}],
         public imgpath:string,
         public ingredients:Ingredients[],
         public id
