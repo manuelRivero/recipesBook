3ffaa0bc31e2093708db168f2c650a94278dc98a
manuel rivero <manuel.enrique.r.v@gmail.com>
2020-05-02 16:33:03 -0300

    [refactor] code cleaning on authService

--- src/app/auth/login.guard.ts
+++ src/app/auth/login.guard.ts
@@ -1,25 +1,24 @@
-import { map, tap } from 'rxjs/operators';
 import { AuthService } from './../services/auth.service';
-import { Injectable, OnInit } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, CanActivate, Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import * as firebase from "firebase";
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate, OnInit {
-  token:string | null=null
-  constructor(private _authService:AuthService, private router:Router){}
-  ngOnInit(){}
+export class LoginGuard implements  CanActivate{
+
+  constructor(private router:Router, private _authService:AuthService){}
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
       let canAccess = false;
       this._authService.userToken$.subscribe( user =>{
-        if(user){
+        if(!user){
           canAccess = true
         }else{
-          this.router.navigate(['/login']);
+          this.router.navigate(['/home']);
         }
      })
      return canAccess;
