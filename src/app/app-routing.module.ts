25bb0cefbd4c8894e4b788340e6f95e423e2aba0
manuel rivero <manuel.enrique.r.v@gmail.com>
2020-05-02 16:48:12 -0300

    [add] login guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,7 @@
+import { HomeComponent } from './components/home/home.component';
 import { AuthGuard } from './auth/auth.guard';
+import { LoginGuard } from './auth/Login.guard';
+
 import { LoginComponent } from './components/login/login.component';
 import { RecipeEditComponent } from './components/recipes/recipe-edit/recipe-edit.component';
 import { RecipePlaceholderComponent } from './components/recipes/recipe-placeholder/recipe-placeholder.component';
@@ -10,15 +13,15 @@ import { ShoppingListComponent } from './components/shopping-list/shopping-list.
 
 
 const routes: Routes = [
-  {path:"", redirectTo:"recipes", pathMatch:"full"},
+  {path:"", redirectTo:"home", pathMatch:"full"},
+  {path:"home", component:HomeComponent, canActivate:[AuthGuard]},
   {path:"recipes", component:RecipesComponent, children:[
     {path:"", component:RecipePlaceholderComponent},
     {path:"new", component:RecipeEditComponent},
     {path:":id", component:RecipesDetailComponent},
     {path:":id/edit", component: RecipeEditComponent}
-
-  ], canActivate: [AuthGuard]},
-  {path:"login", component:LoginComponent},
+  ], canActivate:[AuthGuard]},
+  {path:"login", component:LoginComponent, canActivate:[LoginGuard]},
   {path:"shop", component:ShoppingListComponent, canActivate:[AuthGuard]},
   {path:"**", redirectTo:"recipes"}
 ];
